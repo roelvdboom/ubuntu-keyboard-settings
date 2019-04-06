5c6d7ae86a68a793de7b0349569976de31227fca
Roel van den Boom <roelvandenboom@inogo.nl>
2019-04-06 21:02:49 +0200

    mac and windows scripts

---
+++ mac.sh
@@ -0,0 +1,10 @@
+# clear all options
+setxkbmap -model "pc105" -layout "us,us" -option ""  
+
+# set the Apple keyboard
+setxkbmap -rules "evdev" -model "pc105" -layout "us,us" -option "terminate:ctrl_alt_bksp,lv3:rwin_switch,grp:shifts_toggle,caps:ctrl_modifier,altwin:swap_lalt_lwin"
+
+# swap the grave-tilde with less-greater key
+#  - variant without dead keys
+xmodmap -e "keycode  94 = grave asciitilde"
+xmodmap -e "keycode  49 = less greater less greater bar brokenbar bar brokenbar"
---
+++ windows.sh
@@ -0,0 +1,6 @@
+# clear settings
+setxkbmap -model "pc105" -layout "us,us" -option ""
+
+# pc keyobard
+setxkbmap -rules "evdev" -model "pc105" -layout "us,us" -option "terminate:ctrl_alt_bksp,lv3:rwin_switch,grp:shifts_toggle,caps:ctrl_modifier"
+
