15d621dd4e77b24f864ea843edc906905c82eaa4
Roel van den Boom <roelvandenboom@inogo.nl>
2019-04-08 15:49:10 +0200

    changed path magicmouse.sh

--- magicmouse.sh
+++ magicmouse.sh
@@ -1,4 +1,4 @@
-cd /home/roelvdboom/ubuntu-keyboard-settings/magic-mouse/linux/drivers/hid
+cd /home/roelvdboom/ubuntu-keyboard-settings/magic-mouse-2/linux/drivers/hid
 make
 sudo rmmod hid_magicmouse
 sudo insmod ./hid-magicmouse.ko
